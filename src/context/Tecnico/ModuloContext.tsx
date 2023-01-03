ad4050facc5ea458af44f212213a7991fa64b7d6
Yuri Nunes Atzler <atzleryuri@gmail.com>
2023-01-03 16:24:52 -0300

    merge

--- src/context/Tecnico/ModuloContext.tsx
+++ src/context/Tecnico/ModuloContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from "react";
-import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { API } from "../../utils/api";

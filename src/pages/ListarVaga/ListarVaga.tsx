e0d31251db2ddc86ec23181770fc691aaf357e49
Yuri Nunes Atzler <atzleryuri@gmail.com>
2023-01-04 14:41:50 -0300

    add: paginação na listar vagas

--- src/pages/ListarVaga/ListarVaga.tsx
+++ src/pages/ListarVaga/ListarVaga.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Box, Button } from '@mui/material';
+import { Box, Button, TablePagination } from '@mui/material';
 
 import * as Components from "../../components";
 import { useVaga } from '../../context/Alocacao/VagaContext';
@@ -12,7 +12,7 @@ export const ListarVaga: React.FC = () => {
 
   useEffect(() => {
     pegarVagas();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const buscarPorNomeVaga = async (valor: string) => {
@@ -23,21 +23,27 @@ export const ListarVaga: React.FC = () => {
     await pegarVagas();
   }
 
+  const mudarPagina = async (event: unknown, newPage: number) => {
+    await pegarVagas(newPage)
+  }
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "calc(100vh - 64px)", paddingTop: "80px", paddingBottom: "50px" }}>
-      <Components.Titulo texto="Vagas"/>
+      <Components.Titulo texto="Vagas" />
 
       <Box sx={{ width: { xs: "95%", md: "80%" }, backgroundColor: "var(--branco)", borderRadius: "10px", boxShadow: "10px 10px 10px var(--azul</Box>-escuro-dbc)", padding: "20px" }}>
-        
-        <Box sx={{display: "flex",  width: "100%", alignItems: "center", justifyContent: "space-between", gap: 2, flexDirection: { xs: "column", sm: "row" }}}>
-          <Components.CampoBusca label='Nome' buscar={buscarPorNomeVaga} resetar={resetBuscaVaga}/>
+
+        <Box sx={{ display: "flex", width: "100%", alignItems: "center", justifyContent: "space-between", gap: 2, flexDirection: { xs: "column", sm: "row" } }}>
+          <Components.CampoBusca label='Nome' buscar={buscarPorNomeVaga} resetar={resetBuscaVaga} />
           <Button variant="contained" onClick={() => navigate("/cadastrar-vaga")} sx={{ width: "auto", paddingLeft: "15px", paddingRight: "15px", display: "flex", marginBottom: "10px", textTransform: "capitalize", fontSize: "1rem" }}>Cadastrar Vaga</Button>
         </Box>
 
-        <Box sx={{display: "flex", flexWrap: "wrap", width: "100%", justifyContent: "center", gap: "2rem", mt: 5}}>
-          {vagas?.elementos.map(item => <Components.CardVaga key={item.idVaga} {...item}/>
+        <Box sx={{ display: "flex", flexWrap: "wrap", width: "100%", justifyContent: "center", gap: "2rem", mt: 5 }}>
+          {vagas?.elementos.map(item => <Components.CardVaga key={item.idVaga} {...item} />
           )}
         </Box>
+
+        <TablePagination rowsPerPageOptions={[]} component="div" count={vagas ? vagas.totalElementos : 0} rowsPerPage={vagas ? vagas.tamanho : 0} page={vagas ? vagas.pagina : 0} onPageChange={mudarPagina} />
       </Box>
     </Box>
   )

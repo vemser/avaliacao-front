855a60b55f6376e0d6cabc70f0573f7e71d661b5
Yuri Nunes Atzler <atzleryuri@gmail.com>
2023-01-10 15:45:36 -0300

    add:  componente filtro com debounce

--- src/components/FiltroFeedback/FiltroFeedback.tsx
+++ src/components/FiltroFeedback/FiltroFeedback.tsx
@@ -1,14 +1,29 @@
-import { Autocomplete, Button, TextField } from "@mui/material";
+import { Autocomplete, Button, TextField, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
+import { debounce } from 'lodash';
+import { useEffect } from "react";
+import { useAluno } from "../../context/Comportamental/AlunoContext";
+import { useTrilha } from "../../context/Tecnico/TrilhaContext";
 
 export const FiltroFeedback = () => {
-  const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 }]
+  const { alunos, pegarAluno } = useAluno();
+  const { trilhas, pegarTrilhaFiltroNome, pegarTrilha } = useTrilha();
+
+  const filtroDebounce = debounce((valor, request, option?) => {
+    if (valor) {
+      if (option)
+        request(0, 10, option);
+      else
+        request(valor, 0, 10);
+    } else {
+      request(0, 3);
+    }
+  }, 500)
+
+  useEffect(() => {
+    pegarAluno(0, 3);
+    pegarTrilha(0, 3);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
@@ -16,11 +31,38 @@ export const FiltroFeedback = () => {
         size="small"
         disablePortal
         id="combo-box-demo"
-        options={top100Films}
+        onInputChange={(event, value) => {
+          filtroDebounce(value, pegarAluno, `&nome=${value}`)
+        }}
+        noOptionsText={""}
+        options={alunos ? alunos.elementos.map((aluno) => { return { label: aluno.nome, id: aluno.idAluno } }) : []}
         sx={{ minWidth: 200, display: "flex" }}
-        renderInput={(params) => <TextField {...params} label="Movie" />}
+        renderInput={(params) => <TextField {...params} label="Alunos" />}
       />
 
+      <Autocomplete
+        size="small"
+        disablePortal
+        id="combo-box-demo"
+        onInputChange={(event, value) => {
+          filtroDebounce(value, pegarTrilhaFiltroNome)
+        }}
+        noOptionsText={""}
+        options={trilhas ? trilhas.elementos.map((trilha) => { return { label: trilha.nome, id: trilha.idTrilha } }) : []}
+        sx={{ minWidth: 200, display: "flex" }}
+        renderInput={(params) => <TextField {...params} label="Trilhas" />}
+      />
+
+      <FormControl size="small" sx={{ minWidth: 200, display: "flex" }}>
+        <InputLabel id="selectFeedback">Situação</InputLabel>
+        <Select size="small" label="Situacao" labelId="selectFeedback" defaultValue="" id="situacao">
+          <MenuItem value="initial-situacao" disabled><em>Selecione uma situação</em></MenuItem>
+          <MenuItem id="positivo" value="POSITIVO">Positivo</MenuItem>
+          <MenuItem id="atencao" value="ATENCAO">Atenção</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/*
       <Autocomplete
         size="small"
         disablePortal
@@ -28,25 +70,7 @@ export const FiltroFeedback = () => {
         options={top100Films}
         sx={{ minWidth: 200, display: "flex" }}
         renderInput={(params) => <TextField {...params} label="Movie" />}
-      />
-
-      <Autocomplete
-        size="small"
-        disablePortal
-        id="combo-box-demo"
-        options={top100Films}
-        sx={{ minWidth: 200, display: "flex" }}
-        renderInput={(params) => <TextField {...params} label="Movie" />}
-      />
-
-      <Autocomplete
-        size="small"
-        disablePortal
-        id="combo-box-demo"
-        options={top100Films}
-        sx={{ minWidth: 200, display: "flex" }}
-        renderInput={(params) => <TextField {...params} label="Movie" />}
-      />
+      />  */}
 
       <Button variant="outlined" sx={{ width: "auto", display: "flex", textTransform: "capitalize", fontSize: "1rem" }}>Resetar Filtro</Button>
 
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { debounce } from 'lodash';
+
 export function formatarNomeCompleto(str: string) {
     const words = str.split(" ");
 
@@ -11,3 +13,14 @@ export function formatarNomeCompleto(str: string) {
 export function formatarTexto(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export const filtroDebounce = debounce((valor, request, option?) => {
+    if (valor) {
+      if (option)
+        request(0, 10, option);
+      else
+        request(valor, 0, 10);
+    } else {
+      request(0, 3);
+    }
+  }, 500)
